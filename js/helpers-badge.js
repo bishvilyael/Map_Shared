07c54689a407d4ec1f5584e0a828c47ab778dce5
MaayanChr <maayan.chr@gmail.com>
2026-05-13 17:50:15 +0300

    2

---
+++ js/helpers-badge.js
@@ -0,0 +1,22 @@
+function extractPointDetailsFromDescription(html) {
+  const temp = document.createElement("div");
+  temp.innerHTML = html || "";
+
+  function getValue(label) {
+    const text = temp.innerHTML;
+    const re = new RegExp(`<b>\\s*${label}\\s*:\\s*<\\/b>\\s*([^<]*)`, "i");
+    const m = text.match(re);
+    return m ? m[1].trim() : "";
+  }
+
+  const fbLink = Array.from(temp.querySelectorAll("a"))
+    .map(a => a.getAttribute("href") || "")
+    .find(href => /facebook\.com/i.test(href)) || "";
+
+  return {
+    name: getValue("שם"),
+    date: getValue("תאריך"),
+    place: getValue("מקום"),
+    fbUrl: fbLink
+  };
+}
